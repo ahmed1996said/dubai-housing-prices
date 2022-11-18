e48b3e25fdfb8f0e9b6994437b28ab30f8015e92
Ahmed Al Mahrooqi <ahmed1996said@gmail.com>
2022-11-18 14:40:10 +0400

    Add script for scraping Bayut properties

---
+++ scrape_bayut.py
@@ -0,0 +1,93 @@
+from bs4 import BeautifulSoup
+import requests
+import pandas as pd
+from tqdm import tqdm
+import sys
+
+EMIRATES_VALUES = ['abu-dhabi','dubai','sharjah','ajman','umm-al-quwain','ras-al-khaimah','fujairah']
+NUM_PROPERTIES_PER_PAGE = 24
+
+def get_url(furnished='all',emirate='dubai',page=1):
+    if page == 1:
+        url = f'https://www.bayut.com/to-rent/property/{emirate}/'
+    else:
+        url = f'https://www.bayut.com/to-rent/property/{emirate}/page-{page}/'
+    
+    if furnished == 'furnished':
+        url += '?furnishing_status=furnished'
+    elif furnished == 'unfurnished':
+        url += '?furnishing_status=unfurnished'
+    return url
+
+
+def scrape_bayut(emirate='dubai',furnished='all'): 
+
+    print(f"Starting scrape for {emirate.capitalize()}..")
+    emirate = emirate.lower().replace(' ','-')
+    
+    assert emirate in EMIRATES_VALUES, f'emirate attr must be one of {EMIRATES_VALUES}'
+    assert furnished in ['all','furnished','unfurnished'], f"furnished attr must be one of {['all','furnished','unfurnished']}"
+    
+    bedrooms,bathrooms,area,prices,locations,property_types,property_keywords,furnished_bool = [],[],[],[],[],[],[],[]
+
+    url = get_url(furnished=furnished,emirate=emirate,page=1)
+    
+    html_text = requests.get(url).content
+    soup = BeautifulSoup(html_text,'lxml')
+    num_properties = int(soup.find('span',class_='ca3976f7').text.split(' ')[-2].replace(',',''))
+
+    pages = (num_properties//NUM_PROPERTIES_PER_PAGE) + 1
+    print(f"Found {num_properties} properties with furnished={furnished} ({pages} pages)")
+
+    for page in tqdm(range(1,pages)):
+        try:
+            url = get_url(furnished=furnished,emirate=emirate,page=page)            
+            html_text = requests.get(url).content
+            soup = BeautifulSoup(html_text,'lxml')
+            properties = soup.find_all('div',class_='d6e81fd0')        
+            
+            for property in properties:
+                prices.append(property.find('span',class_='f343d9ce').text)
+                locations.append(property.find('div',class_='_7afabd84').text)
+                property_types.append(property.find('div',class_='_9a4e3964').text)
+                property_keywords.append(property.find('h2',class_='_7f17f34f').text)
+                temp = []
+                for i in property.find('div',class_='_22b2f6ed').children:
+                    temp.append(i.text)
+                try:
+                    bedrooms.append(temp[0])
+                except:
+                    bedrooms.append(-1)
+                try:
+                    bathrooms.append(temp[1])
+                except:
+                    bathrooms.append(-1)
+                try:
+                    area.append(temp[2])
+                except:
+                    area.append(-1)
+                if furnished != 'all':
+                    furnished_bool.append(1 if furnished else 0)
+        except:
+            print(f"Exiting early.. scraped {page-1}/{pages}")
+            break
+
+    col_dict = {
+    "bedrooms" : bedrooms,
+    "bathrooms": bathrooms,
+    "area": area,
+    "prices" : prices,
+    "locations" : locations,
+    "property_types" : property_types,
+    "property_keywords" : property_keywords,
+    "furnished": furnished_bool
+    }
+    if furnished == 'all':
+        del col_dict['furnished']
+    df = pd.DataFrame(col_dict)
+
+    df.to_csv(f'properties_{emirate}_furnished={furnished}2.csv',index=False)
+
+if __name__ == '__main__':
+    scrape_bayut(emirate='dubai',furnished='all')
+
